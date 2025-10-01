7bd9a439e7ba1facff9659bc345d8cb45dd49a2c
sathadev <shell.sathapana@gmail.com>
2025-10-01 17:58:24 +0700

    จัดระเบียบไฟล์

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,17 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'mydb',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
 module.exports = pool;
+
 
